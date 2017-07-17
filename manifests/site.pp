72f8338494e97528e29649da2b9d77e899b591a0
root <root@ip-10-0-3-27.us-west-2.compute.internal>
2017-07-17 18:36:55 +0000

    Almost rebuilt...

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,21 @@
 
+# Fork-Bomb-pm - It's the puppet master...
+node ip-10-0-3-27 {
+
+}
+
+# ForkBombT - Talaba's instance
+node ip-10-0-3-222 {
+
+}
+
+# FORKBOMB!! - Matt's instance
 node ip-10-0-3-181 {
+
+}
+
+# Forkbomb-p - Chris's instance
+node ip-10-0-3-195 {
 
 }
 
---
+++ modules/sshd/manifests/init.pp
@@ -0,0 +1,39 @@
+class sshd {
+
+	package { 'openssh-server':
+		ensure => latest,
+	}
+
+	service { 'sshd':
+		subscribe => File[sshdconfig],
+		require => Package['openssh-server'],
+	}
+
+	file { 'sshdconfig':
+		name => '/etc/ssh/sshd_config',
+		owner => root,
+		group => root,
+		mode => 644,
+		source => 'puppet:///modules/sshd/sshd_config',
+		require => Package['openssh-server'],
+	}
+	
+	ssh_authorized_key { "fork-bomb-cnf":
+				user => "ubuntu",
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCROsSl7lF63moq10Z/JQ4MCEfFwxu/lH2LLTYHl6wolKRSEaJcFNx/Wszf76xTnyjIj9knbFu8OTQtu+zDYm9wwn2yXlULMLOoW76roQivq/zUknhtJYsbnDzCwYHiwdq9eGagn2vqtsZpoZG1vZrvt9SMiNHiyDkeM0yKNM+mi/qd+OWObqsNo0AW/W/5I7IIburbTB6riMBRgmqa5r0jXaQj7bGBImar3ohZRLWCpfH8pFnQpwVSAAAUfJEOCW5Y6dwWXqOh7lfVB1+T9CQsFyLIHyoo/vf+6Nu1D80CPI1IsjKMMuZEraqZrjhc6W9o/Y3+2GtLe0iePN2PA+wR",
+}
+
+	ssh_authorized_key { "Vino-key-pair":
+				user => "ubuntu",
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC5muwuCYjAXyNqJey4F0hheb2KADcYtfLngn2qixILOdeKWNJB8+yGjYKHKc1/39qW55AtucV+77HEWzoYl1/V/0wswmE1RsnOyEce3gcH2naFNcnzKjqnCe0zcZCUBZIr3MsPvR1Y/BwNGCK+Vo7WYyqyOPNAmFj1dNiOEedTKJqzcFIO0awTY5MJC+G9q32YUbHv60vIeWtix1GnaIv9mbjIofYeBXlBr3mxH5dyZxunC+XyYlgCHToJpWlwef9KPQ9mPRsb3Hc98tx9pSpmI6+XHamaoXVuDAQUxEgkwTSth9w7q/S5Kv1rEL/3B8I7a1laT/dd3PPCNtM8s5y/",
+}
+
+	ssh_authorized_key { "":
+				user => "ubuntu",
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC+GfVig6yYm5Zc+5oQR5oCSpn6T2UgY/rEFmCbVPFILHlpmP3Ljm8J7IvDJyfJOZSbYWgt5LZGulpmZYayYFBo02vJC9daA8ax4LgqHmqrOQKl6J+NRwzUbEsQwrYznGwecOrX5d2IG5eV4lon1GAn2rOmZLQGgu6b1F39aYHZ6wRrN1ZAyXbXeX4Jb65Vfd8hbWNQ4/buJlVlJEW2aA0djUEdVNUQuFjZljH9bHk3yM9/Ak4sP9ezB5fNTciKuswvjXLfvl0W0Fds3wAwa+FFMDGfCdZNoigs42Yo533ld1X0fWjC9BvYRNw4k27bAZlX/9x1VcJwGMEQDtueE+aX",
+}
+
+}
