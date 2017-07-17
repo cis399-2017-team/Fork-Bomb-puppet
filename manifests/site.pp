33ebd0ff4a253dfafc70655ad5e2c7d050730f53
root <root@ip-10-0-3-27.us-west-2.compute.internal>
2017-07-17 18:55:55 +0000

    Making sure we're up to date...

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,16 +6,16 @@ node ip-10-0-3-27 {
 
 # ForkBombT - Talaba's instance
 node ip-10-0-3-222 {
-
+	include sshd
 }
 
 # FORKBOMB!! - Matt's instance
 node ip-10-0-3-181 {
-
+	include sshd
 }
 
 # Forkbomb-p - Chris's instance
 node ip-10-0-3-195 {
-
+	include sshd
 }
 
