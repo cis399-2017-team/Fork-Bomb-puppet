cda74d26c75c8183107fa2d42998f352198a0faf
root <root@ip-10-0-3-27.us-west-2.compute.internal>
2017-07-27 05:31:55 +0000

    Added user accounts and keys for all but five members of the class

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,20 +30,307 @@ node ip-10-0-3-243 {
 	include sshd, apache2
 }
 
+group { 'sysadmins':
+  ensure => 'present',
+  gid    => '1018',
+}
+
+user { 'steve':
+  ensure           => 'present',
+  comment          => 'Steve (Professor)',
+  groups           => ['cis399'],
+  home             => '/home/steve',
+  password         => 'LOPSA1',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1025',
+}
+
 group { 'cis399':
   ensure => 'present',
   gid    => '502',
 }
 
+group { 'Ninjas':
+  ensure => 'present',
+  gid    => '1099',
+}
+
+user { 'stevep20':
+  ensure           => 'present',
+  comment          => 'Steve',
+  groups           => ['cis399', 'Ninjas'],
+  home             => '/home/stevep20',
+  password         => '10.0.1.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1002',
+}
+
+user { 'menif':
+  ensure           => 'present',
+  comment          => 'Meni',
+  groups           => ['cis399', 'Ninjas'],
+  home             => '/home/menif',
+  password         => '10.0.1.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1003',
+}
+        
+group { 'BinaryBoiz':
+  ensure => 'present',
+  gid    => '1002',
+}
+
+user { 'levishutts':
+  ensure           => 'present',
+  comment          => 'Levi',
+  groups           => ['cis399', 'BinaryBoiz'],
+  home             => '/home/levishutts',
+  password         => '10.0.2.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1004',
+}
+
+user { 'cplachno':
+  ensure           => 'present',
+  comment          => 'Charles',
+  groups           => ['cis399', 'BinaryBoiz'],
+  home             => '/home/cplachno',
+  password         => '10.0.2.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1005',
+}
+
+user { 'aziz94shh':
+  ensure           => 'present',
+  comment          => 'Aziz',
+  groups           => ['cis399', 'BinaryBoiz'],
+  home             => '/home/aziz94shh',
+  password         => '10.0.2.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1006',
+}
+
+group { 'ForkBomb':
+  ensure => 'present',
+  gid    => '1003',
+}
 
 user { 'cnf':
-	ensure 			=> 'present',
-	home 			=> '/home/stevep20',
-	comment			=> 'Chris Friedrich',
-	groups			=> 'cis399',
-	password		=> '!!',
-	password_max_age	=> '99999',
-	password_min_age	=> '0',
-	shell			=> '/bin/bash',
-	uid			=> '501',
+  ensure           => 'present',
+  comment          => 'Chris Friedrich',
+  groups           => ['sysadmins', 'ForkBomb', 'root'],
+  home             => '/home/cnf',
+  password         => '1SECRET1',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1009',
+  managehome       => true,
 }
+
+user { 'pogrebinsky2010':
+  ensure           => 'present',
+  comment          => 'Talaba Pogrebinsky',
+  groups           => ['sysadmins', 'ForkBomb'],
+  home             => '/home/pogrebinsky2010',
+  password         => '1SECRET1',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1010',
+}
+
+user { 'Vino':
+  ensure           => 'present',
+  comment          => 'Matt',
+  groups           => ['sysadmins', 'ForkBomb'],
+  home             => '/home/Vino',
+  password         => '1SECRET1',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1011',
+}
+
+group { 'HotShame':
+  ensure => 'present',
+  gid    => '1004',
+}
+
+user { 'houllette':
+  ensure           => 'present',
+  comment          => 'Holden',
+  groups           => ['cis399', 'HotShame'],
+  home             => '/home/houllette',
+  password         => '10.0.4.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1024',
+}
+
+user { 'james-dolan':
+  ensure           => 'present',
+  comment          => 'James',
+  groups           => ['cis399', 'HotShame'],
+  home             => '/home/james-dolan',
+  password         => '10.0.4.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1013',
+}
+
+group { 'SegmentationSquad':
+  ensure => 'present',
+  gid    => '1005',
+}
+
+user { 'slynch2':
+  ensure           => 'present',
+  comment          => 'Syd',
+  groups           => ['cis399', 'SegmentationSquad'],
+  home             => '/home/slynch2',
+  password         => '10.0.5.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1014',
+}
+
+user { 'jeremy003':
+  ensure           => 'present',
+  comment          => 'Jeremy',
+  groups           => ['cis399', 'SegmentationSquad'],
+  home             => '/home/jeremy003',
+  password         => '10.0.5.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1015',
+}
+
+user { 'mitchmeabe':
+  ensure           => 'present',
+  comment          => 'Mitch',
+  groups           => ['cis399', 'SegmentationSquad'],
+  home             => '/home/mitchmeabe',
+  password         => '10.0.5.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1016',
+}
+
+group { 'alphabet':
+  ensure => 'present',
+  gid    => '1006',
+}
+
+user { 'nsato10':
+  ensure           => 'present',
+  comment          => 'Naookie',
+  groups           => ['cis399', 'alphabet'],
+  home             => '/home/nsato10',
+  password         => '10.0.6.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1017',
+}
+
+user { 'mister_person':
+  ensure           => 'present',
+  comment          => 'Logan',
+  groups           => ['cis399', 'alphabet'],
+  home             => '/home/mister_person',
+  password         => '10.0.6.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1018',
+}
+
+user { 'mheb':
+  ensure           => 'present',
+  comment          => 'Matt H',
+  groups           => ['cis399', 'alphabet'],
+  home             => '/home/mheb',
+  password         => '10.0.6.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1019',
+}
+
+group { 'HideousSnakes':
+  ensure => 'present',
+  gid    => '1007',
+}
+
+user { 'kaschaefer':
+  ensure           => 'present',
+  comment          => 'Kaela',
+  groups           => ['cis399', 'HideousSnakes'],
+  home             => '/home/kaschaefer',
+  password         => '10.0.7.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1007',
+}
+
+user { 'nboyd':
+  ensure           => 'present',
+  comment          => 'Nate',
+  groups           => ['cis399', 'HideousSnakes'],
+  home             => '/home/nboyd',
+  password         => '10.0.7.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1008',
+}
+
+user { 'coletc':
+  ensure           => 'present',
+  comment          => 'Cole',
+  groups           => ['cis399', 'HideousSnakes'],
+  home             => '/home/coletc',
+  password         => '10.0.7.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1020',
+}
+
+group { 'TheFightingMongooses':
+  ensure => 'present',
+  gid    => '1008',
+}
+
+user { 'dholstege':
+  ensure           => 'present',
+  comment          => 'Davis',
+  groups           => ['cis399', 'TheFightingMongooses'],
+  home             => '/home/dholstege',
+  password         => '10.0.8.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1021',
+}
+
+user { 'jemin':
+  ensure           => 'present',
+  comment          => 'Jemin',
+  groups           => ['cis399', 'TheFightingMongooses'],
+  home             => '/home/jemin',
+  password         => '10.0.8.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1022',
+}
+
+user { 'jsheabia':
+  ensure           => 'present',
+  comment          => 'Joseph',
+  groups           => ['cis399', 'TheFightingMongooses'],
+  home             => '/home/jsheabia',
+  password         => '10.0.8.0/24',
+  password_max_age => '99999',
+  password_min_age => '0',
+  uid              => '1023',
+}
+
--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -18,23 +18,133 @@ class sshd {
 		source => 'puppet:///modules/sshd/sshd_config',
 		require => Package['openssh-server'],
 	}
+
+# Steve
+
+	ssh_authorized_key { "steve-public-key":
+				user => "steve",
+				require => User["steve"],
+				type => ssh-ed25519,
+				key => "AAAAC3NzaC1lZDI1NTE5AAAAILBL3wWzbtQXl6V3saF13p5TM1mdhyrYQZYOqTLw2RT8",
+}
+
+# Fork Bomb
 	
 	ssh_authorized_key { "fork-bomb-cnf":
-				user => "ubuntu",
+				user => "cnf",
+				require => User["cnf"],
 				type => ssh-rsa,
 				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCROsSl7lF63moq10Z/JQ4MCEfFwxu/lH2LLTYHl6wolKRSEaJcFNx/Wszf76xTnyjIj9knbFu8OTQtu+zDYm9wwn2yXlULMLOoW76roQivq/zUknhtJYsbnDzCwYHiwdq9eGagn2vqtsZpoZG1vZrvt9SMiNHiyDkeM0yKNM+mi/qd+OWObqsNo0AW/W/5I7IIburbTB6riMBRgmqa5r0jXaQj7bGBImar3ohZRLWCpfH8pFnQpwVSAAAUfJEOCW5Y6dwWXqOh7lfVB1+T9CQsFyLIHyoo/vf+6Nu1D80CPI1IsjKMMuZEraqZrjhc6W9o/Y3+2GtLe0iePN2PA+wR",
 }
 
 	ssh_authorized_key { "Vino-key-pair":
-				user => "ubuntu",
+				user => "Vino",
+				require => User["Vino"],
 				type => ssh-rsa,
 				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC5muwuCYjAXyNqJey4F0hheb2KADcYtfLngn2qixILOdeKWNJB8+yGjYKHKc1/39qW55AtucV+77HEWzoYl1/V/0wswmE1RsnOyEce3gcH2naFNcnzKjqnCe0zcZCUBZIr3MsPvR1Y/BwNGCK+Vo7WYyqyOPNAmFj1dNiOEedTKJqzcFIO0awTY5MJC+G9q32YUbHv60vIeWtix1GnaIv9mbjIofYeBXlBr3mxH5dyZxunC+XyYlgCHToJpWlwef9KPQ9mPRsb3Hc98tx9pSpmI6+XHamaoXVuDAQUxEgkwTSth9w7q/S5Kv1rEL/3B8I7a1laT/dd3PPCNtM8s5y/",
 }
 
 	ssh_authorized_key { "pogrebinsky-key-pair1":
-				user => "ubuntu",
+				user => "pogrebinsky2010",
+				require => User["pogrebinsky2010"],
 				type => ssh-rsa,
 				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC+GfVig6yYm5Zc+5oQR5oCSpn6T2UgY/rEFmCbVPFILHlpmP3Ljm8J7IvDJyfJOZSbYWgt5LZGulpmZYayYFBo02vJC9daA8ax4LgqHmqrOQKl6J+NRwzUbEsQwrYznGwecOrX5d2IG5eV4lon1GAn2rOmZLQGgu6b1F39aYHZ6wRrN1ZAyXbXeX4Jb65Vfd8hbWNQ4/buJlVlJEW2aA0djUEdVNUQuFjZljH9bHk3yM9/Ak4sP9ezB5fNTciKuswvjXLfvl0W0Fds3wAwa+FFMDGfCdZNoigs42Yo533ld1X0fWjC9BvYRNw4k27bAZlX/9x1VcJwGMEQDtueE+aX",
+}
+
+# Ninjas
+
+	ssh_authorized_key { "stevep20-key-pair":
+				user => "stevep20",
+				require => User["stevep20"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCVa0UoplSUtrseodyA3DLt18HFk9nKfDCAdNfi8BPcxDUfyyhwZrZVOKTQqqY+AWdST1eM1v9VwyCNuzpgjER7Qee7u28d4+fDWzO+XVf+OgpCNxOYuDgm1PWC5pmYcmMVoyfQZkNCZEIdlsG0RR5OQ1sBhugw/OED3u2Sih1gYBxyZBBdHf3liCaVR0aLFi9xB2pgcjnVNootReJXlYleBCl1cS0vBqwEz5HbEgd32vpVjMGoti7D+W3ohXEuJt9Csq6VSwzFj1o4VbncZL4/CJ02JDbOLqKuc7OsyrYDABFXgbJMOtUouX61NdE9QaDM9ZjEsX6XaYVjFZ3U/h1V",
+}
+
+	ssh_authorized_key { "menif-key-pair":
+				user => "menif",
+				require => User["menif"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDKc1y97UibIQujJ9Y25fFYvrpTSck73Cwfh+g7V3YaSvHUvK8Td5tSVH7ue4IFYVEFjy9njJ0h95P6c//Lm5mv0pZl0InOqH0VMN3y1ge6QGz9csTMvrHFsjR9sdQ/eIqTQcohcM/RKVjN3W1oIknogDGK52M2e3SzT+TZNxuaxe/lgg4E6UGD81ZxgKJaZtUEnufhMszRaDCUhq0qaZrhev7PJV1lej1ndfRnGnxgY2F66lISp0TUfxtOtCe7ZUoMtLwmoyB3bwGTpjgOYjTM2DnG1igHQ+nme4VquKMYFbSR7pL6JYtfo8Q1NANMilayDQqcPmZuvXHqO52BL0yh",
+}
+ 
+# Binary Boiz
+
+	ssh_authorized_key { "cplachno-key-pair":
+				user => "cplachno",
+				require => User["cplachno"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCyS6Huzp2PgAPg5RGCLmQrKFQj/MiXOqLHle+Cs/3bEpm7w+Za2OU5hfV10J72U+HQ+9Z7GwYjO1+0iGgfeCTOQqS+JC74ojnmCmw4iMVMII6Bm71eFWYFG/8IB6F9VTUWFx3WW0rPzsdh0qSLaLlLqzHTaOgeEEVWQ2C7IFFKitlqKnfA8m4jg9g+820BBuUwZe9fFY0Nl+vb1sY0FqmxMH1atMNHpTnnS9/jR9fnhuixa/VaSTF7lo+igcUIUl+B5pD+juiim7k9PPJ55p4b9ukujdSNeGruovJUFMIHgp2sRS9OmihlbHOhO1YMlry8/2ex6Fn5Gm6el5Z/KyoH",
+}
+
+# Hot Shame
+
+	ssh_authorized_key { "james-dolan-public-key":
+				user => "james-dolan",
+				require => User["james-dolan"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCzQ9y1oCy32mSQNuxr6ti9aE02mf1TV4DnWAMkHrBhrgzwF6ycLrqkTqUA0gOaWX9AlvgotVNMVgpHicM0QM5BCD1eLai1MKBewhqTzw9hnpaf9OugdWCSQtC7Jc9M0sYFCp6kdLCRHyvUvnb74jQXFA4EiXO1bLfnIbTCcHNvQDdymyEnfHD/Q5JeNm+DqaN4ZqcMqzlnWM27+/m312nn1KEIWpRANWJBJSBIFvXWS4YmrbFZN7WkRYy1GJisGRhk4PaHFQOZJJ26Im8URBhKhPlQwJljIU8kuboQ/2ASrboBPSYOV3Cmoh1PETjeu2OXag1shUlUaSatqBb/t+uF",
+}
+
+# Hideous Snakes 
+
+	ssh_authorized_key { "kaschaefer-public-key":
+                                user => "kaschaefer",
+				require => User["kaschaefer"],
+				type => ssh-rsa,
+ 				key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCLSFcuq/BzCKmFRJ6/aM8ywlUrth1+fSF4R7S7kwevTWN9fjPFULdLGdeWHoNK8gtnoPv8a9mlmW45g4FDGAipR9/yfqPqjKDr3HP62dmwutyiZp7I7iwrsydYjThV29F6TT6R4Mnbm/nmKys75C63EpxguEjWuGhRxe2AzZJMBN5Nvf6I0fA7UdDiAoRArMznyho9Y+Arog1oQO9WdETjil/3vnDRP6S79AvVIochJCdmQWOI9qixPAA1ozxhIgRujjk+iQtj25cbPoCPGSpFYMGvK5vrqWik+rLrkregcbryNuZjr3FeJwd1K/82OqjdwstwE7otvh9YiHUgDO6T",
+}
+
+	ssh_authorized_key { "nboyd-public-key":
+				user => "nboyd",
+				require => User["nboyd"],
+				type => ssh-rsa,
+				key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCUAISsul1Ad4yd8EM3OBSvPh+8vvjkJ0awW8FWX0BfMgKBb/XQ6pyzCA2i5fqw5wC//0F0JCFhh7pin67077Jyq8PdL1c3WbUrJcRMoATtxQi51BfkxnaME0wngKxaiBdzL0Ge2hlmxQhQbiSBQMJvLU2/MjGgi+pGnja2GLzUprqICgBz2cIIDlIryESMr3p9JpfqgjHG6hZZG5hPOrPXenxnmBqSje1ld/9mvc5KCwclbFjtqkKpSFGiCjDgAoAjvuWXtx3sJw0VbborB5QZmyl261E4KDHBDoQpfznP9rx7R49YlM8BHrCAaDyJ3mlV1b50odh1AwxLZgUcTlAB",
+}
+	
+	ssh_authorized_key { "coletc-public-key":
+				user => "coletc",
+				require => User["coletc"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCEmvb0cSCkdIoTggm4y1AtGMLQGupt35HLCwL8ZF147cvRJ72EtU9kdyx4Ol74OqvwizwAVjqCWHDtttrNb1xnmlHAbsG3lpPGday4VXgjfcHAmzmaofKz1BiV/g4f370th92VP82+eBUiDpiF9U/WRVZR5xYvPH02ixY+4T0Tf5wCmG6JTsS6P4SSzEWNU4dkfq9kHTlC7L5BK/2XmdcqpduEz+Fp+5FB5OmHmWnsD1ZPHIN1ExASbHymamUKHTWCCePPgj3J6G0vPFJ4gtfrkDk6OfhX6z9X0jcMvKc4SmtJzIrdRfUpIeIzp9eV8+OB/M2qAo3inYr+pNRFNq1d",
+}
+
+# The Fighting Mongooses
+
+	ssh_authorized_key { "jsheabia-public-key":
+				user => "jsheabia",
+				require => User["jsheabia"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCwP0EXIM1EtCI+OyhgAZ8greAxbdz633f6m/OEsymAtJefkEjw1A5vrMmkyfeoSAELuRbYHxX3N2gPU606IewVZt9KDkL34HMwQDJU6P4fGX5u0WrhmVhqsY2e4UejPtjOtAXGvOjRzkFHuYMLSAL6BbTTTo96Najh9inMxYjb2rP+9E/IRCj9trkEKspHKOTGlj+ADYXtFNb/4KfXuC+oexl2JAGIfSzM9sFBPA3z4d+kxa9AWf1iKWhEB2m5lefD/p8jcjRpXNBm281rIj4vkeWOKFh71gVZ9vR0p+VtYLs3f5tCU2G7zA6pjW8RricBeqG7egWS7DuSLw8datTH",
+}
+
+	ssh_authorized_key { "dholstege-public-key":
+				user => "dholstege",
+				require => User["dholstege"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCTPThmNila82dRmG8nJMBXVReNJT0lrk1saFKmmZ+UjR6t2TvQBErUmZUiA1/dwiIWKRLUF4miagKQMeZECvdPNarL748hlJvKAFibGEDFgTMD3gLnmV5O0dVOaR1yCK2et6RgPr0HaK1YoudllIO3dfcyD14YF7KgkQLUS29GqxzYa8UWfpZXdTAK3i4pGX2zE6FeLohEZT4HscOqa/QQsyrjvuIaWcQCp4YMiFhjMsMIYMsxk2Ge1cbDm+aRQcAIFSUdjeO3R4oMx4GS+WhewpCGVleBn8zSHuh4qBUcQDuMQ4TTJ7mrvNm2tGcHjtuZyKQbhBL1YW1EuS6VsEfZ",
+}
+
+	ssh_authorized_key { "jemin-public-key":
+				user => "jemin",
+				require => User["jemin"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCgq+a6lGdgvfNl37gNfJFzth1aijWIo8goRvH/P2ElQxY1aKmIbYyJKGFkgyEo8cWM+kr4s8OOufputsv6FV4Di9GPECBeBoPN2va4nWakep6Fryanj5QAla7wV0AcVMgiSXYF03+HOLfLBECv3xl3sYleSC3bkkcwgXko8xxal/+qGD63DWWKc4A2WlOf0a8tb6paTPYEUOBBvT4lD5u0yU7wB5+0/HQTGZIqLNX6AeVPMl74zFIhNQbUyjNWMuo3JolOn7JffTzdGBV2nmszxjyAjRG6M2/xVlocpBVjDclhHBGqHqNQd2ZFwWKsY5eZxuk1im4047CUkcFX4vw7",
+}
+
+# alphabet[11:14]
+
+	ssh_authorized_key { "nsato10-public-key":
+				user => "nsato10",
+				require => User["nsato10"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCH7Lspv4byYSdvKzSkh29q6lKco/f7WuiRVg06MeRiRqFDSknppvHoiAap1T3yVs8CH1wiWux9N1B2lEEqUMxFwxdR6vlgKkr8ACOLKVBBX3QPZSv5/jdaPENBqhIOQzQSv35vjNM6atPQ810fpKwoVtemFz4jfXP64GT91HK0oPbEMcTZOHJ2PBSx2l77K6/md/tF0JqK0voL6nz63X3npFuR3qdJ0kxLQghkdzAEryEsuTSn/KxBa2X/fyjfiGQY2h9sEG6fQLFdpTsj3zBmCxyksSUnxR3b+fkfjknJiHiuncu0CzchSbZXodah9zC/UGHo4L0LVAcMeBmvEQYF",
+}
+
+	ssh_authorized_key { "mister_person-public-key":
+				user => "mister_person",
+				require => User["mister_person"],
+				type => ssh-rsa,
+				key => "AAAAB3NzaC1yc2EAAAADAQABAAACAQC2H7AV3pr9nCZfYDHp5Ji1RLQozWYCC9PfodqvEBLY4S+RHsVWMuyenSEdf0mMuYT6QISk2hG5aTZD1psMPK8glFIt0UQ1m8ZPWaUzUogM95WWazwnzfiLcPLAPYRnMBMLO0yFFPlVFSWEOLQ7jMoM/eXOpPzZdZ8iDKa/Fe3tJqnJaNDncxzqvvRjyMWt5+OUidah+DkKWwzymQ+XH/ypIXY5+6bzcBfkb4Z1/ijRbcu7pGu01fWLRWCvy/bJDqBFgSEIGTSngVd/B0oXOGqvdwScgldVoIKExwzSQ7JDITmOTkej/VpwUC3lKM21a+HG85AB5YoPqFr/6ih1FORfQbmjoWdT4VAJoFH6WfngoqcOfci3qa+BNDecRiOuQbNu65ptPfOhJh7aiuDdL+tmY+C00gtbVDhLTYTM5jepembpFcv/7f2O9VtWJpPXZ4u1WkaffmqH3ZqV+/ET7wxDZeGgr6RvoG88UxdWUvr/wHgJvngEz4Y1DHitwCzCXIko0kH73eaIt8kaJUEdOl6FceoBZBK9i2mNktnd5sWtsxAXO/1GOR7a0Mw+XCUI9r8sN6B5fX9wbnBNj2kClEJLInvIVxeQqIw2NH2FzYgxKySCH4h04yoZR0fUSgPHxEdUQPM6X/lZDYX1bVd+NNsu9YQuRH4JLEclE5vTLIXLxw",
 }
 
 }
